f44fa9f30ed8e6ef3a28f155447bb163084154c9
Rafael Melo <contato@rafaelmelo.com.br>
2019-11-01 08:09:05 -0300

    Ajustes

--- lista/views.py
+++ lista/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
+from datetime import datetime
 
 from .models import Lista # Importar models Lista
 from .forms import CadastrarForm  # Importar CadastraForm de forms.py
@@ -8,6 +9,7 @@ from .forms import CadastrarForm  # Importar CadastraForm de forms.py
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     grid = Lista.objects.all()
+    delta = datetime.now().date()
     return render(request, 'lista/index.html', {'grid': grid})
 
 
